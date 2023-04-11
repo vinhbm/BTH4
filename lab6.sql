dba01a9c96afec6d39385591fe5c89a97083249b
vinhbm <75939886+vinhbm@users.noreply.github.com>
2023-04-11 22:35:41 +0700

    Add files via upload

---
+++ lab6.sql
@@ -0,0 +1,92 @@
+﻿USE QLBH
+GO
+---cau1
+CREATE FUNCTION fn_LayThongTinSanPhamTheoHang(@tenhang NVARCHAR(20))
+RETURNS TABLE
+AS
+RETURN
+(
+    SELECT sp.masp, sp.tensp, sp.soluong, sp.mausac, sp.giaban, sp.donvitinh, sp.mota, hsx.tenhang
+    FROM Sanpham sp
+    INNER JOIN Hangsx hsx ON sp.mahangsx = hsx.mahangsx
+    WHERE hsx.tenhang = @tenhang
+)
+SELECT * FROM fn_LayThongTinSanPhamTheoHang('Samsung')
+---cau2
+CREATE FUNCTION fn_DanhSachSanPhamTheoNgayNhap(@ngayx NVARCHAR(10), @ngayy NVARCHAR(10))
+RETURNS TABLE
+AS
+RETURN
+(
+    SELECT sp.tensp, hsx.tenhang, n.soluongN, n.dongiaN
+    FROM Sanpham sp
+    INNER JOIN Hangsx hsx ON sp.mahangsx = hsx.mahangsx
+    INNER JOIN Nhap n ON sp.masp = n.masp
+    WHERE n.ngaynhap BETWEEN @ngayx AND @ngayy
+)
+SELECT * FROM fn_DanhSachSanPhamTheoNgayNhap('2019-03-05', '2020-06-18')
+---cau3
+CREATE FUNCTION fn_GetProductByManufacturer(@manufacturer NVARCHAR(50), @option INT)
+RETURNS TABLE
+AS
+RETURN
+    SELECT sp.masp, sp.tensp, sp.soluong
+    FROM Sanpham sp
+    INNER JOIN Hangsx hs ON sp.mahangsx = hs.mahangsx
+    WHERE hs.tenhang = @manufacturer AND (@option = 0 AND sp.soluong = 0 OR @option = 1 AND sp.soluong > 0)
+SELECT *FROM fn_GetProductByManufacturer('Samsung', 1)
+---câu4
+drop function fn_DanhSachNhanVienTheoPhong
+CREATE FUNCTION fn_DanhSachNhanVienTheoPhong(@tenPhong NVARCHAR(50))
+RETURNS TABLE
+AS
+RETURN 
+(
+    SELECT *
+    FROM Nhanvien
+    WHERE phong = @tenPhong
+)
+
+SELECT * FROM fn_DanhSachNhanVienTheoPhong(N'Vật tư')
+---cau5
+CREATE FUNCTION DanhSachHangSX ( @diachi NVARCHAR(100) )
+RETURNS TABLE
+AS
+RETURN
+    SELECT tenhang, diachi, sodt, email
+    FROM Hangsx
+    WHERE diachi LIKE '%' + @diachi + '%';
+
+SELECT * FROM DanhSachHangSX('KOREA');
+---cau6
+CREATE FUNCTION danh_sach_sp_hangsx_xuat_namxy(@namx int, @namy int)
+RETURNS TABLE
+AS
+RETURN
+    SELECT s.tensp, h.tenhang
+    FROM Sanpham s 
+    JOIN Hangsx h ON s.mahangsx = h.mahangsx
+    JOIN Xuat x ON s.masp = x.masp
+    WHERE YEAR(x.ngayxuat) BETWEEN @namx AND @namy;
+
+SELECT * FROM danh_sach_sp_hangsx_xuat_namxy('2019', '2020')
+
+---cau8
+CREATE FUNCTION fn_NhanVienNhapHang(@ngayNhap DATE)
+RETURNS TABLE
+AS
+RETURN
+SELECT DISTINCT NV.manv, NV.tennv, NV.diachi, NV.sodt, NV.email, NV.phong
+FROM Nhap N
+JOIN Nhanvien NV ON N.manv = NV.manv
+WHERE N.ngaynhap = @ngayNhap
+
+SELECT * FROM fn_NhanVienNhapHang('2020-05-17')
+
+
+
+
+
+
+
+
---
+++ lab7.sql
@@ -0,0 +1,117 @@
+﻿USE QLBH
+GO
+---cau 1
+drop proc sp_InsertHangSX
+CREATE PROCEDURE sp_InsertHangSX
+    @mahangsx VARCHAR(50),
+    @tenhang VARCHAR(50),
+    @diachi VARCHAR(100),
+    @sodt VARCHAR(20),
+    @email VARCHAR(50)
+AS
+BEGIN
+    IF EXISTS (SELECT * FROM HangSX WHERE tenhang = @tenhang)
+    BEGIN
+        PRINT N'Tên hãng đã tồn tại, không thể thêm mới!'
+        RETURN
+    END
+
+    INSERT INTO HangSX(mahangsx, tenhang, diachi, sodt, email)
+    VALUES(@mahangsx, @tenhang, @diachi, @sodt, @email)
+
+    PRINT N'Thêm mới thành công!'
+END
+
+EXEC sp_InsertHangSX 'HSX01', 'Iphone', 'Hà Nội', '0123456709', 'iphone@gmail.com'
+
+---cau2
+Drop proc sp_NhapSanPham
+CREATE PROCEDURE sp_NhapSanPham
+    @masp varchar(10),
+    @tenhangsx varchar(50),
+    @tensp varchar(50),
+    @soluong int,
+    @mausac varchar(20),
+    @giaban decimal(18,2),
+    @donvitinh varchar(10),
+    @mota varchar(200)
+AS
+BEGIN
+    IF EXISTS (SELECT * FROM Sanpham WHERE masp = @masp)
+    BEGIN
+        UPDATE Sanpham
+        SET mahangsx = (SELECT mahangsx FROM Hangsx WHERE tenhang = @tenhangsx),
+            tensp = @tensp,
+            soluong = @soluong,
+            mausac = @mausac,
+            giaban = @giaban,
+            donvitinh = @donvitinh,
+            mota = @mota
+        WHERE masp = @masp
+    END
+    ELSE
+    BEGIN
+        INSERT INTO Sanpham(masp, mahangsx, tensp, soluong, mausac, giaban, donvitinh, mota)
+        VALUES (@masp, (SELECT mahangsx FROM Hangsx WHERE tenhang = @tenhangsx), @tensp, @soluong, @mausac, @giaban, @donvitinh, @mota)
+    END
+END
+
+EXEC sp_NhapSanPham @masp = 'SP001', @tenhangsx = 'Samsung', @tensp = 'Galaxy S21', @soluong = 100, @mausac = 'Xanh', @giaban = 20000000, @donvitinh = 'Cái', @mota = N'Điện thoại thông minh Samsung Galaxy S21'
+
+---cau3
+CREATE PROCEDURE DeleteHangsx
+    @tenhang nvarchar(50)
+AS
+BEGIN
+    SET NOCOUNT ON;
+    
+    -- Kiểm tra hãng sản xuất có tồn tại hay không
+    IF NOT EXISTS (SELECT * FROM Hangsx WHERE tenhang = @tenhang)
+    BEGIN
+        PRINT N'Hãng sản xuất không tồn tại!'
+    END
+    ELSE
+    BEGIN
+        -- Xóa các sản phẩm liên quan đến hãng sản xuất
+        DELETE FROM Sanpham WHERE mahangsx = (SELECT mahangsx FROM Hangsx WHERE tenhang = @tenhang)
+        
+        -- Xóa hãng sản xuất
+        DELETE FROM Hangsx WHERE tenhang = @tenhang
+        
+        PRINT N'Đã xóa hãng sản xuất ' + @tenhang + N' và các sản phẩm liên quan!'
+    END
+END
+
+EXEC DeleteHangsx 'tenhang'
+---câu4
+CREATE PROCEDURE sp_NhapNhanVien
+	@manv INT,
+	@tennv NVARCHAR(50),
+	@gioitinh NVARCHAR(10),
+	@diachi NVARCHAR(100),
+	@sodt NVARCHAR(15),
+	@email NVARCHAR(50),
+	@phong NVARCHAR(50),
+	@flag INT
+AS
+BEGIN
+	IF (@flag = 0)
+	BEGIN
+		UPDATE Nhanvien
+		SET tennv = @tennv,
+			gioitinh = @gioitinh,
+			diachi = @diachi,
+			sodt = @sodt,
+			email = @email,
+			phong = @phong
+		WHERE manv = @manv
+	END
+	ELSE
+	BEGIN
+		INSERT INTO Nhanvien(manv, tennv, gioitinh, diachi, sodt, email, phong)
+		VALUES (@manv, @tennv, @gioitinh, @diachi, @sodt, @email, @phong)
+	END
+END
+
+EXEC sp_NhapNhanVien @manv = 1, @tennv = N'Nguyen Van A', @gioitinh = N'Nam', @diachi = N'Ha Noi', @sodt = N'0987654521', @email = N'abc@gmail.com', 
+	@phong = N'Kinh doanh', @flag = 1
